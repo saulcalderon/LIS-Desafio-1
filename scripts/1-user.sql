a701b5d7924ebb624f563eca3be43cf29b61ddb4
Saul Calderon <saulcalderon1408@gmail.com>
2023-03-17 13:32:09 -0600

    Add user table

---
+++ scripts/1-user.sql
@@ -0,0 +1,17 @@
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    pin INT(4) NOT NULL CHECK (
+        pin BETWEEN 0
+        AND 9999
+    )
+);
+
+INSERT INTO
+    user (name, pin)
+VALUES
+    ('Alice', 1234),
+    ('Bob', 5678),
+    ('Charlie', 2468),
+    ('David', 1357),
+    ('Eve', 9876);
