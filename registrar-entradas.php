57b6f055f91a06cb0cc17e70ec8bfc58229d2b02
Saul Calderon <saulcalderon1408@gmail.com>
2023-03-17 21:21:57 -0600

    Add registrar entradas php file

--- registrar-entradas.php
+++ registrar-entradas.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['user_id'])) {
   <title>Mis Finanzas</title>
 
   <!-- CSS  -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
 </head>
@@ -36,58 +36,59 @@ if (!isset($_SESSION['user_id'])) {
     </div>
   </nav>
 
-  <!-- <ul id="nav-mobile" class="sidenav">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a> -->
-  <!-- </div>
-  </nav> -->
-
   <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
+      <a class="btn-back" href="menu.php"><i class="material-icons medium">arrow_back</i></a>
       <div class="container">
-        <br><br>
-        <h1 class="center">Menú</h1>
-        <h3 class="center bienvenida"></h3>
-        <br><br>
-        <h3 class="center">Elija un servicio</h3>
+        <br /><br />
+        <h1 class="center">Depósito</h1>
+        <br /><br />
+        <h4 class="center">Seleccione o ingrese la cantidad a depositar</h4>
         <div class="row">
-          <div class="col s12 m6">
-            <a class="btn-menu waves-effect waves-light btn-large" href="registrar-entradas.php">Registrar entrada</a>
+          <div class="input-field col m6 offset-m3">
+            <input id="deposit-money-input" type="number" class="form-control txt-margin money-input" aria-label="Monto" placeholder="Monto" min="0.01" step="0.01" />
+            <label class="white-text" for="deposit-money-input">Monto en USD</label>
           </div>
           <div class="col s12 m6">
-            <a class="btn-menu waves-effect waves-light btn-large" href="transactions.html">Ver entradas</a>
+            <a id="btn-add-25" class="btn-menu waves-effect waves-light btn-large">$25</a>
           </div>
           <div class="col s12 m6">
-            <a class="btn-menu waves-effect waves-light btn-large" href="withdraw.html">Registrar salida</a>
+            <a id="btn-add-50" class="btn-menu waves-effect waves-light btn-large">$50</a>
           </div>
           <div class="col s12 m6">
-            <a class="btn-menu waves-effect waves-light btn-large" href="services.html">Ver salidas</a>
+            <a id="btn-add-100" class="btn-menu waves-effect waves-light btn-large">$100</a>
           </div>
+          <div class="col s12 m6">
+            <a id="btn-add-200" class="btn-menu waves-effect waves-light btn-large">$200</a>
+          </div>
+        </div>
+        <div class="row">
           <div class="col m6 offset-m3">
-            <a class="btn-menu waves-effect waves-light btn-large" href="services.html">Mostrar balance</a>
+            <a id="btn-deposit" class="btn-menu waves-effect waves-light btn-large">Depositar</a>
           </div>
         </div>
       </div>
     </div>
-    <div class="parallax"><img id="bc-image" src="background4.jpg" alt=""></div>
+    <div class="parallax">
+      <img id="bc-image" src="background4.jpg" alt="" />
+    </div>
   </div>
 
   <footer class="page-footer teal">
     <div class="footer-copyright">
       <div class="container">
-        <a class="brown-text text-lighten-3">Pokemon Bank ©</a> Copyright 2022 - LIC
+        <a class="brown-text text-lighten-3">Pokemon Bank ©</a> Copyright 2022
+        - LIC
       </div>
     </div>
   </footer>
 
-
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   <script src="js/materialize.js"></script>
+  <script src="js/jspdf.min.js"></script>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   <script src="js/init.js"></script>
-
 </body>
 
 </html>
